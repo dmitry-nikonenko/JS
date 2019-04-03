bcffb3badc6dd5d9ee7014813e0ad43a45b5c38a
dmitry_nikonenko <dnikonenko88@gmail.com>
2019-04-03 09:12:08 +0300

    module-01

---
+++ module_01/js2/script.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// let day = 'monday';
+// let number = +prompt('Enter number');
+// if(number ===1){
+//     day='monday';
+//     alert('monday')
+// } else if(number===2){
+//     day='thusady';
+//     alert('thusady')
+// } else if (number===3){
+//     alert('wednesday')
+// } else{
+//     console.error('error')
+// }
+// switch(number){
+//     case 1:
+//     alert('monday');
+//     break;
+//     case 2:
+//     alert('thusady');
+//     break;
+//     case 3:
+//     alert('wednesday');
+//     break;
+//     default:
+//     console.error('error')
+// }
+// 
+ 
