9f9bce6a7a76339d1c8ec0c3ac7bf12817e1af42
dmitry_nikonenko <dnikonenko88@gmail.com>
2019-04-03 17:02:31 +0300

    ver.1

--- module_02/js_1/script.js
+++ module_02/js_1/script.js
@@ -1,1 +1,37 @@
 'use strict'
+
+//  const arr = [1,2,3,4,5];
+ 
+
+// //  console.log(arr.length)
+// //  console.log(arr[2])
+
+// // console.log(arr[arr.length -1])
+
+// const word = 'hello my friends';
+// // const word2 = word.split(' ');
+// // delete word2[1];
+// // console.log(word2.join('='));
+
+// console.log(arr.indexOf(4));
+// console.log(arr.includes(4))
+
+// arr.push(8,7)
+// console.log(arr)
+
+
+// const lg ={en:['q','w'],
+// ru:['й','ц']};
+// console.log(lg.en)
+
+
+// const arr = [[1,2,3],[4,5,6]];
+// console.log(arr[0][2])
+
+// const arr = [1,2,3,4,5];
+
+// const newArr = arr.slice();
+
+// newArr.splice(1,1, 'php','ruby')
+
+// console.log(newArr)
