231136266b67331bb7b82f2dd3adb30c62753a61
dmitry_nikonenko <dnikonenko88@gmail.com>
2019-04-04 13:33:55 +0300

    ver.2

--- module_02/js_2/script.js
+++ module_02/js_2/script.js
@@ -1,1 +1,62 @@
 'use strict'
+
+// let count = 1;
+// while(count < 10){
+//     console.log(count);
+//     count = +1;
+// }  
+
+// let mass = [];
+// let arrS;
+
+// do{
+//     arrS = (prompt('console'));
+//     mass.push(arrS)
+// } while (arrS !== null);
+// console.log(mass)
+
+// let newMass =[]
+// for(let i = 0; i < mass.length; i +=1){
+//    if (mass[i][0] === 'M' || mass[i][0] === 'B' )
+//    newMass.push(mass[i]);
+//    console.log(newMass);
+// }
+
+
+// const users = ["Mango", "Poly", "Ajax", "Chelsey"];
+// for(let i = 0; i<users.length; i+=1){
+//     console.log(users[i])
+// }
+
+// let newMass =[]
+
+// const mass = ["Mango", "Poly", "Ajax", "Chelsey"];
+// for(let i = 0; i<mass.length; i+=1){
+//     if(mass[i].length<5){
+//         newMass.push(mass[i])
+//     }
+// }
+// console.log(newMass)
+
+// let newMass =[]
+
+// const mass = ["Mango", "Poly", "Ajax", "Chelsey"];
+// for(let i = 0; i<mass.length; i+=1){
+//     if(mass[i] === "Ajax" ) break;
+//         console.log(mass[i])}
+
+
+
+// let newMass =[]
+// const mass = ["Mango", "Poly", "Ajax", "Chelsey"];
+
+// for(let key of mass){
+//     if(key.length>5){ console.log(key)}
+//     }
+
+
+// let counter =3;
+// do {
+//     let pas =Number(prompt('Enter'));
+//     counter = counter -1;
+// } while(counter)
