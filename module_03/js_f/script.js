5391fbe4f3d78352ffb189f01d0ef02ebddf3d3c
dmitry_nikonenko <dnikonenko88@gmail.com>
2019-04-05 14:23:42 +0300

    macd

--- module_03/js_f/script.js
+++ module_03/js_f/script.js
@@ -1,1 +1,51 @@
 'use strict'
+
+// const add = function(a,b,c){return a+b+c}
+// console.log(add(10,15,20));
+
+
+// const name = function(name, lastName){console.log(`my name is ${name} my last name ${lastName}`)}
+// name("dima","Nikon")
+
+
+
+
+
+
+// name();
+// age(15,15); 
+// console.log(age(15,15));
+
+// function name (){
+//     console.log('Dima');
+// }
+// function age (a,b) {
+//     return a + b;
+// }    
+
+
+
+
+// function fn(b) {
+//     return b % 2 === 0;
+// }
+
+// function fn2(b) {
+//     if (fn(b)) {
+//         alert('oK')
+//     } else {
+//         alert('No')
+//     }
+// }
+
+// fn2(4)
+
+// -------------------------------
+
+// const fn = (a,b) => a+b;
+// console.log  (fn(3,3))
+
+
+
+
+ 
