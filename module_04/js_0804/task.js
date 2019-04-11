0084f969a0e28678a26b13c460a31e4e9bf1c044
dmitry_nikonenko <dnikonenko88@gmail.com>
2019-04-11 13:42:34 +0300

    homeWork

--- module_04/js_0804/task.js
+++ module_04/js_0804/task.js
@@ -493,45 +493,124 @@
 //     cheese: 1
 //   };
   
-//   /* Пример использования */
-//   const mango = new Cashier('Mango', products);
-  
-//   // Проверяем исходные значения полей
-//   console.log(mango.name); // Mango
-//   console.log(mango.productDatabase); // ссылка на базу данных продуктов (объект products)
-//   console.log(mango.customerMoney); // 0
-  
-//   // Вызываем метод countTotalPrice для подсчета общей суммы
-//   // передавая order - список покупок пользователя
-//   const totalPrice = mango.countTotalPrice(order);
-  
-//   // Проверям что посчитали
-//   console.log(totalPrice); // 110
-  
-//   // Вызываем setCustomerMoney для запроса денег покупателя
-//   mango.setCustomerMoney(300);
-  
-//   // Проверяем что в поле с деньгами пользователя
-//   console.log(mango.customerMoney); // 300
-  
-//   // Вызываем countChange для подсчета сдачи
-//   const change = mango.countChange();
-  
-//   // Проверяем что нам вернул countChange
-//   console.log(change); // 190
-  
-//   // Проверяем результат подсчета денег
-//   if(change !== null) {
-//      // При успешном обслуживании вызываем метод onSuccess
-//     mango.onSuccess(change); // Спасибо за покупку, ваша сдача 190
-//   } else {
-//     // При неудачном обслуживании вызываем метод onError   
-//     mango.onError(); // Очень жаль, вам не хватает денег на покупки
-//   }
-  
-//   // Вызываем reset при любом исходе обслуживания
-//   mango.reset();
-  
-//   // Проверяем значения после reset
-//   console.log(mango.customerMoney); // 0
 
+
+const products = {
+    bread: 10,
+    milk: 15,
+    apples: 20,
+    chicken: 50,
+    cheese: 40,
+};
+
+const order = {
+    bread: 2,
+    milk: 2,
+    apples: 1,
+    cheese: 1,
+};
+
+const cashier = {
+    name: 'Mango',
+    customerMoney: 0,
+    totalPrice: 0,
+    change: 0,
+    error: null,
+    greet() {
+        return console.log(`Добрый день, вас обслуживает ${this.name}`);
+    },
+    getCustomerMoney(value) {
+        return this.customerMoney = value;
+    },
+    countTotalPrice(allProducts, order) {
+
+        for (let values in allProducts) {
+
+            const keysOrder = Object.keys(order);
+
+
+            if (keysOrder.includes(values)) {
+
+                this.totalPrice += allProducts[values] * order[values];
+
+            }
+        }
+
+        return this.totalPrice;
+    },
+    countChange() {
+
+        this.change = this.customerMoney - this.totalPrice;
+
+        return this.change;
+    },
+    onSuccess() {
+
+        const equalsTotalPrice = this.customerMoney === this.totalPrice ?
+            console.log('Спасибо за покупку!') :
+            console.log(`Спасибо за покупку, ваша сдача ${this.change}!`);
+
+        return equalsTotalPrice;
+
+    },
+    onError() {
+        return console.log('Очень жаль, вам не хватает денег на покупки');
+
+    },
+    reset() {
+
+        this.customerMoney = 0;
+        this.totalPrice = 0;
+        this.change = 0;
+        this.error = null;
+    }
+}
+
+
+// Проверяем исходные значения полей
+console.log(cashier.name); // Mango
+console.log(cashier.customerMoney); // 0
+console.log(cashier.totalPrice); // 0
+console.log(cashier.change); // 0
+console.log(cashier.error); // null
+
+cashier.greet(); // Добрый день, вас обслуживает Mango
+
+// Вызываем метод countTotalPrice для подсчета общей суммы
+// передавая products - список всех продуктов и order - список покупок клиента
+cashier.countTotalPrice(products, order);
+
+// Проверям что посчитали
+console.log(cashier.totalPrice); // 110
+
+// Вызываем getCustomerMoney для запроса денег клиента
+cashier.getCustomerMoney(300);
+
+// // Проверяем что в поле с деньгами клиента
+console.log(cashier.customerMoney); // 300
+
+// // Вызываем countChange для подсчета сдачи
+cashier.countChange();
+
+// // Проверяем что нам вернул countChange
+console.log(cashier.change); // 190
+
+// Проверяем результат подсчета денег
+if (cashier.customerMoney >= cashier.totalPrice && cashier.error === null) {
+
+    // При успешном обслуживании вызываем метод onSuccess
+    cashier.onSuccess(); // Спасибо за покупку, ваша сдача 190
+} else {
+    // При неудачном обслуживании вызываем метод onError
+    cashier.onError(); // Очень жаль, вам не хватает денег на покупки
+};
+
+
+// Вызываем reset при любом исходе обслуживания
+cashier.reset();
+
+// Проверяем значения после reset
+console.log(cashier.customerMoney); // 0
+console.log(cashier.totalPrice); // 0
+console.log(cashier.change); // 0
+console.log(cashier.error); // null
