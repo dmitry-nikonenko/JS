ae29f81cf13006887cb4ddd123e7c00d92c4bdde
dmitry_nikonenko <dnikonenko88@gmail.com>
2019-04-12 16:13:37 +0300

    ver.1

---
+++ module_06/js.11.04/script.js
@@ -0,0 +1,77 @@
+'use strict'
+
+// const he = {
+//     name:'Bond',
+//     number: 7,
+//     getName(){console.log(this.number)},
+    
+// }
+// he.getName()
+// // const she = {
+// //     name:'Julia',
+// //     age: 32,
+// // }
+
+// const she = Object.create(he);
+// she.name = 'Maria Ivanovna';
+// she.age = 45;
+// she.boobsPower = 4;
+
+// // he.__proto__=she;
+
+// console.log(he)
+// console.log(she)
+
+// for(let key in she){if(she.hasOwnProperty(key))console.log(key)}
+
+// function Dragon(name,level){
+//     this.name = name;
+//     this.level = level;
+// }
+// Dragon.prototype.getLevel = function(el){return this.level += el;}
+
+// const dragon = new Dragon('bond', 80);
+ 
+// dragon.getLevel(10);
+
+// function DubleDragon(name,level,weapon){this.weapon =weapon;  Dragon.call(this,name,level)}
+// DubleDragon.prototype.boom = function(value){return this.name=value}
+
+// const duble = new DubleDragon('Vasja', 9999,'falos');
+// duble.boom("Vahtang")
+// console.log(duble) 
+// console.log(dragon)
+// const dragon2 = new Dragon('Shalashovka', 99)
+// console.log(dragon2)
+
+// //----------------------------------------------------------------------------------------------------------------
+
+// class MyClass{
+//     constructor(name,xp,level){
+//         this.name=name;
+//         this.xp=xp;
+//         this.level=level;
+//     }
+//     getName(){console.log(this.name)}
+//     minus(el){return this.xp-=el}
+// }
+// const myclass = new MyClass("Dusja",100,80);
+// myclass.getName()
+// myclass.minus(10)
+
+// console.log(myclass)
+
+// //-----------------------------------------------------------------------------------------------------------------
+
+// class NewClass extends MyClass {
+//     constructor(weapon){super(); this.weapon=weapon}
+// }
+// const newclad=new NewClass('Pterodactel')
+// console.log(newc)
+
+
+
+
+
+
+ 
