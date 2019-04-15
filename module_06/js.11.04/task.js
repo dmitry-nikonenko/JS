d9d3769994b55ff899f35ba4be9555b925584244
dmitry_nikonenko <dnikonenko88@gmail.com>
2019-04-15 14:11:23 +0300

    homework

--- module_06/js.11.04/task.js
+++ module_06/js.11.04/task.js
@@ -11,15 +11,362 @@
   а использовать его смогут все экземпляры, по ссылке.
   
   Создать несколько экземпляров с разными значениями свойств, вывесди их в консоль.
-*/
-function Account(login,email){
-    this.login=login;
-    this.email=email;
-    Account.prototype.getInfo=function(){
-        console.log(this.login,this.email)
-    }
-}
-const account = new Account('Mangozedog', 'mango@dog.woof');
+// */
+// function Account(login,email){
+//     this.login=login;
+//     this.email=email;
+//     Account.prototype.getInfo=function(){
+//         console.log(this.login,this.email)
+//     }
+// }
+// const account = new Account('Mangozedog', 'mango@dog.woof');
 
-console.log(Account.prototype.getInfo); // function
-account.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+// console.log(Account.prototype.getInfo); // function
+// account.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+//-----------------------------------------------------------------------------------------------------------------
+
+
+/*
+  Напиши ES6 класс StringBuilder.
+  
+  На вход (в конструкторе) он получает один параметр string - строку,
+  которую записывает в свойство _value.
+  
+  Добавь классу следующие методы:
+  
+    - геттер value - возвращает текущее значение поля _value
+  
+    - append(str) - получает парметр str (строку) и добавляет ее в конец _value
+    
+    - prepend(str) - получает парметр str (строку) и добавляет ее в начало value
+  
+    - pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
+*/
+
+
+// class StringBuilder{constructor(str){this._value=str;}
+// get value(){return this._value};
+// append(str){return this._value += str}
+// prepend(str){return this._value =str+=this._value}
+// pad(str){return this._value=str+this._value+str;}
+
+// }
+// console.log(StringBuilder.prototype)
+// const builder = new StringBuilder('.');
+
+// builder.append('^'); 
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^'); 
+// console.log(builder.value); // '^.^'
+
+// builder.pad('='); 
+// console.log(builder.value); // '=^.^='
+
+
+//---------------------------------------------------------------------------------------------------------------
+// class Car {
+//   constructor({
+//     maxSpeed = 0,
+//     speed = 0,
+//     running = false,
+//     distance = 10,
+//     price
+//   }) {
+
+//     this.maxSpeed = maxSpeed;
+//     this.speed = speed;
+//     this.running = running;
+//     this.distance = distance;
+//     this.price=price;
+
+//   }
+
+
+
+//   turnOn(running) {
+//     return this.running = true;
+//   };
+
+//   turnOff(running) {
+//     return this.running = false;
+//   };
+
+//   accelerate(spd) {
+//     if (this.spd <= this.maxSpeed) {
+//       this.speed = spd;
+//       return this.speed
+//     }
+//   };
+
+//   decelerate(spd) {
+//     if (this.spd < this.maxSpeed && spd > 0) {
+//       this.speed = spd;
+//       return this.speed
+//     }
+//   };
+
+//   drive(hours) {
+//     if (this.distance = hours * this.speed) {} else if (this.running) {
+//       return this.distance
+//     }; 
+    
+    
+
+//   };
+//   static getSpecs(el) {
+//     console.log(`speed${el.speed},${el.speed},${el.maxSpeed},${el.distance}`);
+  
+// }
+
+// }
+
+// const car = new Car({
+//   maxSpeed: 100
+// });
+
+// console.log(car.turnOn())
+// // // console.log(car.turnOff())
+// console.log(car.accelerate(105))
+// // console.log(car.decelerate())
+// console.log(car.drive(5))
+// console.log(car)
+
+//---------------------------------------------------------------------------------------------------------------
+
+/*
+ * Добавь к классу Car из предыдущего задания статический
+ * метод getSpecs, который принимает объект-машину как параметр
+ * и выводит в консоль значения полей maxSpeed, speed, running и distance.
+ */
+
+// car.turnOn();
+// car.accelerate(50);
+// car.drive(2);
+
+// Car.getSpecs(car); // maxSpeed: 100, speed: 50, running: true, distance: 100
+
+//--------------------------------------------------------------------------------------------------------------
+
+/*
+ * Добавь классу Car свойство цены автомобиля, назови его сам.
+ * Добавь геттер и сеттер value, который будет работать с свойством цены автомобиля.
+ */
+// class Car{constructor(maxSpeed=0,price=0){
+// this.maxSpeed=maxSpeed;
+// this.price=price;
+// }
+// get value(){return this.price};
+// set value(value){return this.price=value}
+// }
+// const car = new Car({ maxSpeed: 50, price: 2000 });
+// console.log(car.value); // 2000
+
+// car.value = 4000;
+// console.log(car.value); // 4000
+// console.log(car)
+
+
+// -----------------------------------------  HOME Work------------------------------------------------------
+
+/*
+  Сеть фастфудов предлагает несколько видов гамбургеров.
+  Основа (булочка) гамбургера может быть большой или маленькой (обязательно):
+	- маленькая (+30 денег, +50 калорий)
+	- большая (+50 денег, +100 калорий)
+  Гамбургер может быть с одной из нескольких видов начинок (обязательно):
+	- сыром (+15 денег, +20 калорий)
+	- салатом (+20 денег, +5 калорий)
+	- мясом (+35 денег, +15 калорий)
+  Дополнительно, гамбургер можно:
+	- посыпать приправой (+10 денег, +0 калорий)
+	- полить соусом (+15 денег, +5 калорий)
+  Напишите скрипт, расчитывающий стоимость и калорийность гамбургера. Используте ООП подход,
+  создайте класс Hamburger, константы, методы для выбора опций и рассчета нужных величин.
+  Написанный класс должен соответствовать следующему jsDoc описанию. То есть класс должен содержать
+  указанные методы, которые принимают и возвращают данные указанного типа.
+*/
+
+/**
+ * Класс, объекты которого описывают параметры гамбургера.
+ */
+class Hamburger {
+ 
+  constructor(size, stuffing) {
+    this._size = size;
+    this._stuffing = stuffing;
+    this._toppings = [];
+  }
+
+  /**
+   * Добавить topping к гамбургеру. Можно добавить несколько topping, при условии, что они разные.
+   * @param {String} topping - Тип добавки
+   */
+  addTopping(topping) {if(this._toppings.includes(topping)){return} this._toppings.push(topping); console.log(this._toppings)}
+
+  /**
+   * Убрать topping, при условии, что она ранее была добавлена
+   * @param {String} topping - Тип добавки
+   */
+  removeTopping(topping) {
+		if (this._toppings.includes(topping)) {
+			this._toppings = this._toppings.filter(el => el !== topping);
+		}
+	}
+
+  /**
+   * Получить список toppings
+   * @returns {Array} - Массив добавленных topping, содержит значения констант Hamburger.TOPPING_*
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.toppings и нам вернет массив добавок
+   */
+  get toppings() {return this._toppings;}
+
+  /**
+   * Узнать размер гамбургера
+   * @returns {String} - размер гамбургера
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.size и нам вернет размер
+   */
+  get size() {return this._size;}
+
+  /**
+   * Узнать начинку гамбургера
+   * @returns {String} - начинка гамбургера
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.stuffing и нам вернет начинку
+   */
+  get stuffing() {return this._stuffing;}
+
+  /**
+   * Узнать цену гамбургера
+   * @returns {Number} - Цена в деньгах
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.price и нам вернет сумму.
+   */
+  get price() {
+		return (
+			Hamburger.STUFFINGS[this.stuffing].price +
+			Hamburger.SIZES[this.size].price +
+			this.toppings.reduce(
+				(acc, el) => acc + Hamburger.TOPPINGS[el].price,
+				0
+			)
+		);
+	}
+  /**
+   * Узнать калорийность
+   * @returns {Number} - Калорийность в калориях
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.calories и нам вернет сумму.
+   */
+  get calculateCalories() {
+		return (
+			Hamburger.STUFFINGS[this.stuffing].calories +
+			Hamburger.SIZES[this.size].calories +
+			this.toppings.reduce(
+				(acc, el) => acc + Hamburger.TOPPINGS[el].calories,
+				0
+			)
+		);
+	}
+}
+
+/*
+  Размеры, виды добавок и начинок объявите как статические поля класса.
+  Добавьте отсутсвующие поля по аналогии с примером.
+*/
+Hamburger.SIZE_SMALL = 'SIZE_SMALL';
+Hamburger.SIZE_LARGE = 'SIZE_LARGE';
+
+Hamburger.SIZES = {
+  [Hamburger.SIZE_SMALL]: {
+    price: 30,
+    calories: 50,
+  },
+  [Hamburger.SIZE_LARGE]: {
+    price: 50,
+    calories: 100,
+  }
+};
+
+
+
+Hamburger.STUFFING_CHEESE = 'STUFFING_CHEESE';
+Hamburger.STUFFING_SALAD = 'STUFFING_SALAD';
+Hamburger.STUFFING_MEAT = 'STUFFING_MEAT';
+
+Hamburger.STUFFINGS = {
+  [Hamburger.STUFFING_CHEESE]: {
+    price: 15,
+    calories: 20,
+  },
+  [Hamburger.STUFFING_SALAD]: {
+    price: 20,
+    calories: 5,
+  },
+  [Hamburger.STUFFING_MEAT]: {
+    price: 35,
+    calories: 15,
+  },
+  
+};
+
+Hamburger.TOPPING_SPICE = 'TOPPING_SPICE';
+Hamburger.TOPPING_SAUCE = 'TOPPING_SAUCE';
+
+Hamburger.TOPPINGS = {
+  [Hamburger.TOPPING_SPICE]: {
+    price: 10,
+    calories: 0,
+  },
+  [Hamburger.TOPPING_SAUCE]: {
+    price: 15,
+    calories: 5,
+  },
+};
+
+/* Вот как может выглядеть использование этого класса */
+
+// Маленький гамбургер с начинкой из сыра
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+
+// Добавка из приправы
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+
+// Спросим сколько там калорий
+console.log("Calories: ", hamburger.calculateCalories);
+
+// Сколько стоит?
+console.log("Price: ", hamburger.price);
+
+// Я тут передумал и решил добавить еще соус
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+
+// А сколько теперь стоит?
+console.log("Price with sauce: ", hamburger.price);
+
+// Проверить, большой ли гамбургер?
+console.log("Is hamburger large: ", hamburger.size === Hamburger.SIZE_LARGE); // -> false
+
+// Убрать добавку
+hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+
+// Смотрим сколько добавок
+console.log("Hamburger has %d toppings", hamburger.toppings.length); // 1
+
+/*
+  🔔 Обратите внимание на такие моменты:
+    	✔️ класс не взаимодействует с внешним миром. Это не его дело, этим занимается
+    		другой код, а класс живет в изоляции от мира
+    	✔️ обязательные параметры (размер и начинка) мы передаем через конструктор,
+		чтобы нельзя было создать объект, не указав их
+    	✔️ необязательные (добавка) добавляем через методы
+    	✔️ типы начинок обозначены "константами" с понятными именами (на самом деле просто
+	    	свойствами, написанным заглавными буквами, которые мы договорились считать "константами")
+    	✔️ объект создается через конструктор - функцию, которая задает начальные значения полей.
+    	✔️ в свойствах объекта гамбургера логично хранить исходные данные (размер, тип начинки),
+      		а не вычисленные из них (цена, число калорий и т.д.). Рассчитывать цену и калории
+		логично в тот момент, когда это потребуется, а не заранее.
+*/
